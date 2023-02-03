f8f10acc645b9305b732e775a488d40a93b009bb
MattGaarder <matteus.gaarder@gmail.com>
2023-02-03 14:32:59 +0000

    still messing with keys

--- assets/keys2.js
+++
@@ -1,4 +0,0 @@
-var OMDBKey = "407da853";
-var compromisedYoutubeApiKey = "AIzaSyArZC4w_ZrUpGsejJW0TBItsl8bm2JQMTs"; //compromised
-var youtubeApiKey = "AIzaSyAe24J-pt7WWNHFvLHpof0uEOP3liLg4dk";
-var OMDBKey = "407da853"; 
