a3859d0cf8e5a06d746afcbc16bee038da6c6ee3
MattGaarder <matteus.gaarder@gmail.com>
2023-02-06 17:00:17 +0000

    makes separate poster div for responsive .details p

--- assets/script.js
+++ assets/script.js
@@ -39,9 +39,10 @@ function displayInfo(OMDBCall) {
     playerDiv = $("<div id='player'>");
     buttonDiv = $("<div id='buttonDiv'>");
     discoverDiv = $("<div class='discover'>");
-    seenButton = $("<button id='seen'>");
-    watchButton = $("<button id='watch'>");
-    infoButton = $("<i class='fa-solid fa-circle-info'></i>")
+    posterDiv = $("<div class='poster'>");
+    seenButton = $("<i id='seen' class='fa-solid fa-eye'></i>")
+    watchButton = $("<i id='watch' class='fa-solid fa-square-plus'></i>");
+    // infoButton = $("<i class='fa-solid fa-circle-info'></i>")
     seenButton.text("Seen");
     watchButton.text("Watch");
     detailsDiv = $("<div class='details'>");
@@ -70,7 +71,7 @@ function displayInfo(OMDBCall) {
     // console.log(movieObject);
     // console.log(OMDBCall.Title);
     var imageURL = OMDBCall.Poster;
-    discoverDiv.css("background-image", "url(" + imageURL + ")");
+    // discoverDiv.css("background-image", "url(" + imageURL + ")");
     $(".discover").children("#player").remove();
     $(".discover").children("iframe").remove();
     $(".discover").removeClass("active");
@@ -78,13 +79,14 @@ function displayInfo(OMDBCall) {
     
         discoverDiv.addClass("active");
         detailsDiv.addClass("active");
-    // discoverDiv.append(moviePoster);
+        posterDiv.css("background-image", "url(" + imageURL + ")");
+    discoverDiv.append(posterDiv);
     discoverDiv.append(detailsDiv);
     discoverDiv.append(playerDiv);
     detailsDiv.append(buttonDiv);
     buttonDiv.append(watchButton);
     buttonDiv.append(seenButton);
-    buttonDiv.append(infoButton);
+    // buttonDiv.append(infoButton);
     movieObject = {
         Title: OMDBCall.Title,
         Year: OMDBCall.Year,
@@ -215,48 +217,71 @@ function moveToList(movieObject, list) {
 
 function createSeenArray() {
     var seenArray = JSON.parse(localStorage.getItem("seenArray")) || [];
-    for(var i = 0; i < seenArray.length; i++) {
-        var seenItem = $("<li>");
-        seenItem.text(seenArray[i].Title);
-        seenItem.addClass('seen-item');
-        seenItem.attr("data-title", seenArray[i].Title);
-        seenItem.attr("data-year", seenArray[i].Year);
-        seenItem.attr("data-poster", seenArray[i].Poster);
-        seenItem.attr("data-videoid", seenArray[i].videoId);
-        deleteBtn = $("<i class='fa-solid fa-circle-xmark'></i>");
-        deleteBtn.addClass("delete-item-btn")
-        seenItem.append(deleteBtn);
-        infoBtn = $("<i class='fa-solid fa-circle-info'></i>");
-        infoBtn.addClass("info-btn")
-        seenItem.append(infoBtn);
-        seenList.append(seenItem);
-    }
+    // for(var i = 0; i < seenArray.length; i++) {
+    //     var seenItem = $("<li>");
+    //     seenItem.text(seenArray[i].Title);
+    //     seenItem.addClass('seen-item');
+    //     seenItem.attr("data-title", seenArray[i].Title);
+    //     seenItem.attr("data-year", seenArray[i].Year);
+    //     seenItem.attr("data-poster", seenArray[i].Poster);
+    //     seenItem.attr("data-videoid", seenArray[i].videoId);
+    //     deleteBtn = $("<i class='fa-solid fa-circle-xmark'></i>");
+    //     deleteBtn.addClass("delete-item-btn")
+    //     seenItem.append(deleteBtn);
+    //     infoBtn = $("<i class='fa-solid fa-circle-info'></i>");
+    //     infoBtn.addClass("info-btn")
+    //     seenItem.append(infoBtn);
+    //     seenList.append(seenItem);
+    createLists (seenArray, seenList);
+    // }
 };
 createSeenArray();
 
-function createWatchArray() {
-    var watchArray = JSON.parse(localStorage.getItem("watchArray")) || [];
-    for(var i = 0; i < watchArray.length; i++) {
-        var watchItem = $("<li>");
-        watchItem.text(watchArray[i].Title);
-        watchItem.addClass("watch-item");
-        watchItem.attr("data-title", watchArray[i].Title);
-        watchItem.attr("data-year", watchArray[i].Year);
-        watchItem.attr("data-poster", watchArray[i].Poster);
-        watchItem.attr("data-videoid", watchArray[i].videoId);
-        // watchItem.append('<button class="btn btn-danger btn-small delete-item-btn">Remove</button>');
-        // watchItem.append('<button class="btn btn-danger btn-small seen-item-btn">Seen</button>');
+function createLists(array, parent) {
+    for(var i = 0; i < array.length; i++) {
+        var item = $("<li>");
+        item.text(array[i].Title);
+        item.addClass("watch-item");
+        item.attr("data-title", array[i].Title);
+        item.attr("data-year", array[i].Year);
+        item.attr("data-poster", array[i].Poster);
+        item.attr("data-videoid", array[i].videoId);
         deleteBtn = $("<i class='fa-solid fa-circle-xmark'></i>");
         deleteBtn.addClass("delete-item-btn")
-        watchItem.append(deleteBtn);
+        item.append(deleteBtn);
         seenBtn = $("<i class='fa-solid fa-eye'></i>")
         seenBtn.addClass("seen-item-btn")
-        watchItem.append(seenBtn);
+        item.append(seenBtn);
         infoBtn = $("<i class='fa-solid fa-circle-info'></i>");
         infoBtn.addClass("info-btn")
-        watchItem.append(infoBtn);
-        watchList.append(watchItem);
+        item.append(infoBtn);
+        parent.append(item);
     }
+}
+
+function createWatchArray() {
+    var watchArray = JSON.parse(localStorage.getItem("watchArray")) || [];
+    // for(var i = 0; i < watchArray.length; i++) {
+    //     var watchItem = $("<li>");
+    //     watchItem.text(watchArray[i].Title);
+    //     watchItem.addClass("watch-item");
+    //     watchItem.attr("data-title", watchArray[i].Title);
+    //     watchItem.attr("data-year", watchArray[i].Year);
+    //     watchItem.attr("data-poster", watchArray[i].Poster);
+    //     watchItem.attr("data-videoid", watchArray[i].videoId);
+    //     deleteBtn = $("<i class='fa-solid fa-circle-xmark'></i>");
+    //     deleteBtn.addClass("delete-item-btn")
+    //     watchItem.append(deleteBtn);
+    //     seenBtn = $("<i class='fa-solid fa-eye'></i>")
+    //     seenBtn.addClass("seen-item-btn")
+    //     watchItem.append(seenBtn);
+    //     infoBtn = $("<i class='fa-solid fa-circle-info'></i>");
+    //     infoBtn.addClass("info-btn")
+    //     watchItem.append(infoBtn);
+    //     watchList.append(watchItem);
+    // }
+    console.log("creating watch list");
+    createLists (watchArray, watchList);
 };
 createWatchArray();
 
@@ -264,7 +289,6 @@ var watchArray;
 
 function moveToSeen(event) {
     var seenArray = JSON.parse(localStorage.getItem("seenArray"));
-
     var moveToSeenObject = {
         Title: $(event.target).parent().data("title")
     }
@@ -304,19 +328,34 @@ function removeItem(event) {
     for(var i = 0; i < listArray.length; i++) {
         if(listArray[i].Title === $(event.target).parent().data("title")){
             listArray.splice(i, 1);
+            
             break;
         }
     } 
     localStorage.setItem(whichList + "Array", JSON.stringify(listArray));
     // this function works fine up until this point
-    if(whichList === "seen"){
-        clearArray(seenList);
-        createSeenArray();
-    } else {
-        clearArray(watchList);
-        createWatchArray();
-    }
+    removeBtn.parent('li').remove();
+    // if(whichList === "seen"){
+    //     clearArray(seenList);
+    //     createSeenArray();
+    // } else {
+    //     clearArray(watchList);
+    //     createWatchArray();
+    // }
 };
+
+
+
+
+
+
+// Look up filter method.
+
+
+
+
+
+
 // console.log(whichList + "List");
 
 // createWatchArray();
