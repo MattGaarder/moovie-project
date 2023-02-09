0953c1882c698d21c26931b1d53d031aec92fd82
MattGaarder <matteus.gaarder@gmail.com>
2023-02-09 17:17:47 +0000

    deletes git ignore so that page is functional with key

---
+++ assets/keys3.js
@@ -0,0 +1,5 @@
+var OMDBKey = "407da853";
+// var compromisedYoutubeApiKey = "AIzaSyArZC4w_ZrUpGsejJW0TBItsl8bm2JQMTs"; //compromised
+// var compromisedYoutubeApiKey2 = "AIzaSyAe24J-pt7WWNHFvLHpof0uEOP3liLg4dk";
+var OMDBKey = "407da853"; 
+var youtubeApiKey = "AIzaSyAw7R_5sHFdALiY0BUgiwLGdbsIEvyMNOw"
