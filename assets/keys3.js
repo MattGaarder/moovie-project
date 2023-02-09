f5c5288f53dc7857ec2b280c27187d757bfe269a
MattGaarder <matteus.gaarder@gmail.com>
2023-02-09 17:25:06 +0000

    changes key3 file so that pages might work

--- assets/keys3.js
+++ assets/keys3.js
@@ -2,4 +2,5 @@ var OMDBKey = "407da853";
 // var compromisedYoutubeApiKey = "AIzaSyArZC4w_ZrUpGsejJW0TBItsl8bm2JQMTs"; //compromised
 // var compromisedYoutubeApiKey2 = "AIzaSyAe24J-pt7WWNHFvLHpof0uEOP3liLg4dk";
 var OMDBKey = "407da853"; 
-var youtubeApiKey = "AIzaSyAw7R_5sHFdALiY0BUgiwLGdbsIEvyMNOw"
+// var youtubeApiKey (tired) = "AIzaSyAw7R_5sHFdALiY0BUgiwLGdbsIEvyMNOw" //tired
+var youtubeApiKey = "AIzaSyA1-PgZ7gNMfgr_3Fp39k3iP9K7uYMKQbo" //Sorry Anna, hope you don't mind that this goes up here (I will remove tomorrow, just need to check that this is functional and my key has exceeded API daily quota)
